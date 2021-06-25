13550529823fa6664f47dc044d2f8f12e2f1e43a
RyuheiYanamoto <yanamoto0513@gmail.com>
2021-06-25 11:42:28 +0900

    最初のコミットです

---
+++ src/test/java/jp/ac/hcs/s3a329/WebportalApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.s3a329;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
