06f0e2a7478ae65f257133283bd8a3a9fa29e5e4
RyuheiYanamoto <yanamoto0513@gmail.com>
2021-06-29 10:42:21 +0900

    2021/06/29 1時間目

---
+++ src/main/java/jp/ac/hcs/s3a329/zipcode/ZipCodeEntity.java
@@ -0,0 +1,25 @@
+package jp.ac.hcs.s3a329.zipcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+
+/**
+ * 郵便番号情報検索結果の店舗情報
+ * 各項目のデータ使用については、APIの使用を参照とする
+ * 1つの郵便番号に複数の住所が紐づくこともあるため、リスト構造となっている
+ * -http://zipcloud.ibsnet.co.jp/doc/api
+ *
+ */
+@Data
+public class ZipCodeEntity {
+	/**ステータス*/
+	private String status;
+	
+	/**メッセージ*/
+	private String message;
+	
+	/**郵便番号情報のリスト*/
+	private List<ZipCodeData> results = new ArrayList<ZipCodeData>();
+}
